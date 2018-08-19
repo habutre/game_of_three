19aa21c3449b02ef023263ea5a3d14dfd0c997f1
Rogério Ramos da Silva <habutre@gmail.com>
2018-08-19 15:00:10 +0200

    Add initial game rules implementation
    This commit add a bare rules for the game of three, improvements will
    come later adding player names, ID just to mention some ideas around my
    mind.

--- lib/game.ex
+++ lib/game.ex
@@ -1,3 +1,26 @@
 defmodule Game do
+  @moduledoc """
+  The Game module is responsible by evaluate the player's movements
+  and announce the winner or pass the turn for the opponent
+  """
 
+  def evaluate_move(), do: {:error, "An empty movement is not allowed"}
+
+  def evaluate_move(value = nil), do: {:error, "An empty movement is not allowed"}
+
+  def evaluate_move(value) when is_bitstring(value),
+    do: {:error, "A numeric value is expected here"}
+
+  def evaluate_move(value) do
+    cond do
+      div(value, 3) == 1 ->
+        {:winner, "The player xxx has won!"}
+
+      div(value, 3) < 1 ->
+        {:tie, "There is no winners in this game!"}
+
+      true ->
+        {:ok, div(value, 3)}
+    end
+  end
 end
--- test/game_test.exs
+++ test/game_test.exs
@@ -2,7 +2,30 @@ defmodule GameTest do
   use ExUnit.Case
   doctest Game
 
-  test "should start the game stadium" do
-    refute(true, "Implementation missing!") 
+  test "GIVEN a player move WHEN no value is sent                           THEN an error message should be sent" do
+    assert({:error, "An empty movement is not allowed"} = Game.evaluate_move())
+  end
+
+  test "GIVEN a player move WHEN the value is null                          THEN an error message should be sent" do
+    assert({:error, "An empty movement is not allowed"} = Game.evaluate_move(nil))
+  end
+
+  test "GIVEN a player move WHEN the value is not a number                  THEN an error message should be sent" do
+    assert({:error, "A numeric value is expected here"} = Game.evaluate_move("a"))
+  end
+
+  test "GIVEN a player move WHEN the value divided by 3 is less or equals 1 THEN there is not Winner" do
+    assert({:tie, "There is no winners in this game!"} = Game.evaluate_move(2))
+  end
+
+  test "GIVEN a player move WHEN the value divided by 3 is equals 1         THEN announce the Winner" do
+    assert({:winner, "The player xxx has won!"} = Game.evaluate_move(3))
+  end
+
+  test "GIVEN a player move WHEN the value divided by 3 is greather than 1  THEN send the changed value to opponent" do
+    assert({:ok, 12} = Game.evaluate_move(38))
+    assert({:ok, 345} = Game.evaluate_move(1037))
+    assert({:ok, 2906} = Game.evaluate_move(8719))
+    refute({:ok, 1} == Game.evaluate_move(3))
   end
 end
