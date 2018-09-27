5db4077bdaeca79707c9747a030e3736550ead70
Rogério Ramos <habutre+github@gmail.com>
2018-09-27 09:27:01 +0200

    WIP: restruct the infra
    The game logic will not be affected by such decisions

--- lib/game_of_three/infrastructure/game_manager.ex
+++ lib/game_of_three/infrastructure/game_manager.ex
@@ -1,12 +1,14 @@
 defmodule GameOfThree.Infrastructure.GameManager do
-  use GenStage
+  use GenServer
 
   @moduledoc """
-  The GameManager module is responsible by manage the games and players playing.
+  The GameManager module is responsible by manage the games and players.
   Each player will be identified by an PID as well as the games
   Multiple games are expected to be played at same time
   """
 
+  ## Client API
+  #
   def init(args) do
     {:producer, args}
   end
@@ -17,5 +19,20 @@ defmodule GameOfThree.Infrastructure.GameManager do
     IO.inspect(player)
     IO.inspect(move)
     {:no_reply, player, move}
+  end
+
+  ## Server Callbacks
+  def init(:ok) do
+    {:ok, %{}}
+  end
+
+  def handle_call({:add_player, name}, _from, game) do
+    # game struct game_name, current_turn{}, players
+    # add player to player list
+    {:reply, Map.fetch(players, name), game}
+  end
+
+  def handle_cast({:player, name, :turn, value}, game) do
+    {:reply, 
   end
 end
