a07b582b3d0305572c0bdff5746a20acc07073f0
Rogério Ramos da Silva <habutre@gmail.com>
2018-11-04 13:48:30 +0100

    Remove unnecessary file

--- lib/game_of_three/domain/manager.ex
+++
@@ -1,5 +0,0 @@
-# A Genserver should know about how the game is managed?
-# No no no, we have to create a game management
-# * manage players, two initially
-# * manage the turns, who should play each time, who is the current sender
-# * manage when notifify the end-user/ui/cli about the game end, start
